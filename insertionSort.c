f5f7f5b2fd02c20ca13ee08463882c2133f08cc1
Prakhar Maurya <109527284+nuke100pr@users.noreply.github.com>
2024-02-03 17:06:12 +0530

    Update insertionSort.c

--- insertionSort.c
+++ insertionSort.c
@@ -6,7 +6,7 @@ void insertionSort(int arr[], int n) {
         key = arr[i];
         j = i - 1;
 
-        // Move elements of arr[0..i-1] that are greater than key to one position ahead of their current position
+        
         while (j >= 0 && arr[j] > key) {
             arr[j + 1] = arr[j];
             j = j - 1;
